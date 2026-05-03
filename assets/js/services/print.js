24aa556c8e3e5403ab1a8af1e87e6a4739663ffa
ekoredu-maker <ekoredu@gmail.com>
2026-05-03 16:38:43 +0900

    Delete assets directory

--- assets/js/services/print.js
+++
@@ -1,9 +0,0 @@
-export function showPrintable(html) {
-  const root = document.getElementById('printRoot');
-  root.innerHTML = html;
-  document.body.classList.add('printing');
-  window.print();
-  setTimeout(() => {
-    document.body.classList.remove('printing');
-  }, 300);
-}
--- assets/js/templates/reports.js
+++
@@ -1,3 +0,0 @@
-export function reportVersionLabel() {
-  return 'V10.3';
-}
